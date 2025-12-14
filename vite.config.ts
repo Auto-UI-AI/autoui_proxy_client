4977f576426bd2e840690fc4bdac44693d263e20
IvanDeresh <dereshivan0@gmail.com>
2025-12-14 21:57:17 +0100

    feat: font added, docs and othe updates

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,9 @@ import { defineConfig } from "vite";
 import path from "path";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
-import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 
-// https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), tailwindcss(), cssInjectedByJsPlugin()],
+    plugins: [react(), tailwindcss()],
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
@@ -18,20 +16,8 @@ export default defineConfig({
             "@widgets": path.resolve(__dirname, "./src/widgets"),
         },
     },
-
     build: {
-        lib: {
-            entry: path.resolve(__dirname, "./src/lib/index.js"),
-            name: "@packs/campaign-manager",
-            fileName: (format) => {
-                if (format === "es") {
-                    return `index.js`;
-                }
-                return `index.cjs`;
-            },
-        },
-        rollupOptions: {
-            treeshake: true,
-        },
+        outDir: "dist",
+        sourcemap: false,
     },
 });
